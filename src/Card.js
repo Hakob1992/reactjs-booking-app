68672cdde4612d295e48c36a636106c38762065f
Hakob Hakobyan <hakobhakobyan1992@yahoo.com>
2020-08-29 23:37:41 +0400

    search part

---
+++ src/Card.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Card.css'
+
+export const Card = ({ src, title, description, price }) => {
+   return (
+      <div className='card'>
+
+      </div>
+   )
+}
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Home.css'
+import { Banner } from './Banner'
+import { Card } from '@material-ui/core'
 
 export const Home = () => {
    return (
       <div className='home'>
-         <h1>HOME</h1>
+         <Banner />
+
+         <div className="home__section">
+            <Card />
+         </div>
       </div>
    )
 }
